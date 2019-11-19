7a1f2919f0f6976569228b92a82b1c22fb5f75a4
AIZAWA Hina <hina@fetus.jp>
2019-11-19 16:48:36 +0900

    Use INTL_IDNA_VARIANT_UTS46 instead of INTL_IDNA_VARIANT_2003

--- src/IdnToPunycodeFilterValidator.php
+++ src/IdnToPunycodeFilterValidator.php
@@ -11,6 +11,10 @@ namespace jp3cki\yii2\validators;
 
 use yii\validators\FilterValidator;
 
+use function idn_to_ascii;
+
+use const INTL_IDNA_VARIANT_UTS46;
+
 /**
  * The filter validator which converts IDN to Punycoded domain name
  */
@@ -19,29 +23,36 @@ class IdnToPunycodeFilterValidator extends FilterValidator
     /** @inheritdoc */
     public function init()
     {
-        $this->filter = function ($value) {
+        $this->filter = function (string $value): string {
             if (strpos($value, '/') === false) {
-                return strtolower(idn_to_ascii($value));
+                return strtolower(static::idnToAscii($value));
             }
+
             if (strpos($value, '//') !== false) {
                 return preg_replace_callback(
                     '!(?<=//)([^/:]+)!',
-                    function ($match) {
-                        return strtolower(idn_to_ascii($match[1]));
+                    function (array $match): string {
+                        return strtolower(static::idnToAscii($match[1]));
                     },
                     $value,
                     1
                 );
             }
+
             return preg_replace_callback(
                 '!^([^/:]+)!',
-                function ($match) {
-                    return strtolower(idn_to_ascii($match[1]));
+                function (array $match): string {
+                    return strtolower(static::idnToAscii($match[1]));
                 },
                 $value,
                 1
             );
         };
         parent::init();
+    }
+
+    protected static function idnToAscii(string $value): string
+    {
+        return idn_to_ascii($value, 0, INTL_IDNA_VARIANT_UTS46);
     }
 }
